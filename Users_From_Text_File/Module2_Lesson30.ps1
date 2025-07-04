35c7f5a6791bf536ef79bb9aed99932da4b38ada
Qhamaninande Rebe <67279129+QueMaster@users.noreply.github.com>
2025-07-04 12:12:52 +0200

    Add files via upload

---
+++ Users_From_Text_File/Module2_Lesson30.ps1
@@ -0,0 +1,14 @@
+﻿$UsersToImport = Get-Content -Path 'C:\test\users.txt'
+
+foreach($Item in $UsersToImport)
+{
+    
+    $SecureString = ConvertTo-SecureString -String 'Pa55w.rd' -AsPlainText -Force
+
+    $OU = 'OU=test,DC=Root,DC=Local'
+
+
+    New-ADuser -Name $Item -AccountPassword $SecureString -Description 'Mordor' -Enabled $true -Path $OU
+
+
+}
